cf818aa27422cb54a8f48b28b7f99bfce05e371d
ndeberuti <ndeberuti@gmail.com>
2021-04-17 18:02:15 -0300

    Price estimation service using exported service function

--- src/server/controller/priceEstimationController.ts
+++ src/server/controller/priceEstimationController.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import * as priceEstimationService from '../../services/priceEstimationService'
+import { priceEstimationService } from '../../services/priceEstimationService'
 
 export const checkPrice = async (req: express.Request, res: express.Response, 
     next: express.NextFunction) => {
     try {
-        await priceEstimationService.estimate();
+        
+        await priceEstimationService();
         
         return res.status(200).json({status: req.params});
     } catch (error) {
