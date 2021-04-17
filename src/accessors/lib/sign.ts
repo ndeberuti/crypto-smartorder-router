81087842e1371e428f8da6c9a9f0409d30cfb4fa
ndeberuti <ndeberuti@gmail.com>
2021-04-17 18:01:45 -0300

    Improve signer to take into account query params

--- src/accessors/lib/sign.ts
+++ src/accessors/lib/sign.ts
@@ -1,12 +1,21 @@
 import config from 'config'
 import CryptoJS from 'crypto-js'
 
-export const sign = (timestamp: string, method: string, endpoint: string ): string => {
+export const sign = (timestamp: string, method: string, endpoint: string, params: Object ): string => {
    const secretKey: string = config.get('secretKey');
-    
-    const preSignedString = timestamp + method+ endpoint;
+    const endpointWithParams = addParamsToEndpoint(endpoint, params);
+    const preSignedString = timestamp + method + endpointWithParams;
     const signedString =  CryptoJS.HmacSHA256(preSignedString, secretKey);
     const signedStringInBase64 = CryptoJS.enc.Base64.stringify(signedString);
 
     return signedStringInBase64;
+}
+
+const addParamsToEndpoint = (endpoint: string, params: any): string => {
+    let endpointWithParams = endpoint;
+    for (const param in params) {
+        endpointWithParams = endpointWithParams + '?' + param + '=' + params[param];
+    }
+    
+    return endpointWithParams;
 }
