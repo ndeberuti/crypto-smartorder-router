a3c62ab4f4ef6359a216c0e303d18df111ef085e
ndeberuti <ndeberuti@gmail.com>
2021-04-18 16:54:57 -0300

    Price estimation service now returns orderId

--- src/services/priceEstimationService.ts
+++ src/services/priceEstimationService.ts
@@ -1,8 +1,13 @@
-import { getPriceEstimation } from '../accessors/builder'
+import { getPriceEstimation, executeSwapOrder } from '../accessors/builder'
+import { DatabaseClient } from '../clients/databaseClient';
+import { PriceEstimation } from '../interfaces/priceEstimation';
+const databaseInstance = DatabaseClient.getInstance();
 
-export const priceEstimationService = async () => {
-    const res = await getPriceEstimation();
-    
-    return res;
+export const priceEstimationService = async (clientId: string, priceEstimation: PriceEstimation): Promise<any> => {
+    const price = await getPriceEstimation(priceEstimation);
+   
+    const orderId = databaseInstance.savePrice(clientId, priceEstimation.pair, priceEstimation.side, price);
+
+    return { status: 200, data: {orderId}};
 }
 
