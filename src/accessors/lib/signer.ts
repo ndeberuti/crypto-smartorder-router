a8836fa3d4b32580402c7e50bfe680801dbb63cb
ndeberuti <ndeberuti@gmail.com>
2021-04-18 13:30:04 -0300

    Rename signer and add logic to support params and body into signature

--- src/accessors/lib/signer.ts
+++ src/accessors/lib/signer.ts
@@ -1,10 +1,10 @@
 import config from 'config'
 import CryptoJS from 'crypto-js'
 
-export const sign = (timestamp: string, method: string, endpoint: string, params: Object ): string => {
+export const sign = (timestamp: string, method: string, endpoint: string, params: Object | undefined, body: Object | undefined ): string => {
    const secretKey: string = config.get('secretKey');
     const endpointWithParams = addParamsToEndpoint(endpoint, params);
-    const preSignedString = timestamp + method + endpointWithParams;
+    const preSignedString = timestamp + method + endpointWithParams + (body !== undefined ? JSON.stringify(body) : '');
     const signedString =  CryptoJS.HmacSHA256(preSignedString, secretKey);
     const signedStringInBase64 = CryptoJS.enc.Base64.stringify(signedString);
 
@@ -13,8 +13,15 @@ export const sign = (timestamp: string, method: string, endpoint: string, params
 
 const addParamsToEndpoint = (endpoint: string, params: any): string => {
     let endpointWithParams = endpoint;
+    let first: Boolean = true;
     for (const param in params) {
-        endpointWithParams = endpointWithParams + '?' + param + '=' + params[param];
+        if (first) {
+            endpointWithParams = endpointWithParams + '?' + param + '=' + params[param];
+            first = false;
+        } else {
+            endpointWithParams = endpointWithParams + '&' + param + '=' + params[param];
+        }
+
     }
     
     return endpointWithParams;
