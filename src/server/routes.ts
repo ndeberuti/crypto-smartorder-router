beb642f51ab796ecf311711b0faa2ac5d78c2c13
ndeberuti <ndeberuti@gmail.com>
2021-04-18 13:59:24 -0300

    Add route for swap-order

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import * as healthController from './controller'
-import * as priceEstimationController from './controller'
+import * as healthController from '../server/controller/healthController'
+import * as priceEstimationController from '../server/controller/priceEstimationController'
+import * as swapOrderController from '../server/controller/swapOrderController'
 
 export const bind = (app: express.Application): void => {
     app.get('/health', healthController.check)
     app.get('/prime-estimation', priceEstimationController.checkPrice)
+    app.post('/swap-order', swapOrderController.swap)
 }
