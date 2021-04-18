e9838d41f751d2273e1099addec483ee3967e251
ndeberuti <ndeberuti@gmail.com>
2021-04-18 16:56:04 -0300

    Add client id header to priceEstimationService

--- src/server/controller/priceEstimationController.ts
+++ src/server/controller/priceEstimationController.ts
@@ -1,13 +1,19 @@
 import express from 'express'
+import { PriceEstimation } from '../../interfaces/priceEstimation';
 import { priceEstimationService } from '../../services/priceEstimationService'
 
 export const checkPrice = async (req: express.Request, res: express.Response, 
     next: express.NextFunction) => {
     try {
+
+        const priceEstimation: PriceEstimation = req.body;
+        const clientId: any = req.headers['x-client-id'];
         
-        await priceEstimationService();
+        const {status, data} = await priceEstimationService(clientId, priceEstimation);
         
-        return res.status(200).json({status: req.params});
+        console.log('Controller res',JSON.stringify(data));
+
+        res.status(status).json(data);
     } catch (error) {
         next(error)
     }
