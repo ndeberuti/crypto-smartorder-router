1c41851a763943b8e1e70dc6926bdb8919f2d8a9
ndeberuti <ndeberuti@gmail.com>
2021-04-18 21:35:42 -0300

    Add apply to swapOrderController

--- src/server/controller/swapOrderController.ts
+++ src/server/controller/swapOrderController.ts
@@ -1,14 +1,13 @@
 import express from 'express'
 import { SwapOrder } from '../../interfaces/swapOrder';
-import { swapOrderService } from '../../services/swapOrderService'
+import { applyOrderService, swapOrderService } from '../../services/orderService'
 
 export const swap = async (req: express.Request, res: express.Response, 
     next: express.NextFunction) => {
     try {
         const swapOrder: SwapOrder = req.body;
-
+       
         console.log('RQ body', JSON.stringify(req.body));
-
         console.log('SWAP order', swapOrder);
         
         const {status, data} = await swapOrderService(swapOrder);
@@ -20,6 +19,24 @@ export const swap = async (req: express.Request, res: express.Response,
             return res.status(400).json(data)
         }
 
+        res.status(status).json(data);
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const apply = async (req: express.Request, res: express.Response, 
+    next: express.NextFunction) => {
+    try {
+        const orderId: string = req.params.id;
+
+        console.log('RQ body', JSON.stringify(req.body));
+
+        const {status, data} = await applyOrderService(orderId);
+
+
+        console.log('Controller res',JSON.stringify(data));
+        
         res.status(status).json(data);
     } catch (error) {
         next(error)
