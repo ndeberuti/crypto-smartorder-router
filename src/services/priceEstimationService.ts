cfbe1e326c045cff5d60965bd6330619dd691078
ndeberuti <ndeberuti@gmail.com>
2021-04-18 21:36:48 -0300

    Add persistance of orders to price estimation service

--- src/services/priceEstimationService.ts
+++ src/services/priceEstimationService.ts
@@ -1,13 +1,13 @@
 import { getPriceEstimation, executeSwapOrder } from '../accessors/builder'
 import { DatabaseClient } from '../clients/databaseClient';
 import { PriceEstimation } from '../interfaces/priceEstimation';
-const databaseInstance = DatabaseClient.getInstance();
 
 export const priceEstimationService = async (clientId: string, priceEstimation: PriceEstimation): Promise<any> => {
     const price = await getPriceEstimation(priceEstimation);
-   
-    const orderId = databaseInstance.savePrice(clientId, priceEstimation.pair, priceEstimation.side, price);
 
-    return { status: 200, data: {orderId}};
+    const databaseInstance = await DatabaseClient.getInstance();
+    const orderId = await databaseInstance.saveOrder(clientId, priceEstimation.pair, priceEstimation.side, priceEstimation.volume, price);
+
+    return { status: 200, data: {orderId, price}};
 }
 
