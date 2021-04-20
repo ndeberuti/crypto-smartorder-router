8f971ecf0de96ad5877463db21f26a8e018e8c80
ndeberuti <ndeberuti@gmail.com>
2021-04-20 01:41:16 -0300

    Rename endpoint

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -4,8 +4,9 @@ import * as optimalPriceController from './controller/optimalPriceController'
 import * as orderController from './controller/orderController'
 
 export const bind = (app: express.Application): void => {
+    // TODO use camelCase
     app.get('/health', healthController.check)
-    app.post('/price-estimation', optimalPriceController.optimalPrice)
+    app.post('/optimal-price', optimalPriceController.optimalPrice)
     app.post('/swap-order', orderController.swap)
     app.post('/apply-order/:id', orderController.apply)
 }
