94974e189705f073deb5bf8c1754022efe4ed7d3
ndeberuti <ndeberuti@gmail.com>
2021-04-20 01:41:06 -0300

    Export app

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,7 +3,7 @@ import {bind} from './routes';
 
 const app = express();
 const port = 3001;
-app.use(express.json())
-bind(app)
-app.listen(port)
+app.use(express.json());
+bind(app);
 
+export = app.listen(port);
