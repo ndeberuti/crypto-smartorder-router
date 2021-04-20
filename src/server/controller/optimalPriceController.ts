fb852cb57057d3dd2df31eaf068c26619730099d
ndeberuti <ndeberuti@gmail.com>
2021-04-20 01:42:32 -0300

    WIP optimalPriceController working on runtime but not in tests

--- src/server/controller/optimalPriceController.ts
+++ src/server/controller/optimalPriceController.ts
@@ -11,10 +11,9 @@ export const optimalPrice = async (req: express.Request, res: express.Response,
         const clientId = typeof req.headers['x-client-id'] === 'string' ? req.headers['x-client-id']: '';
 
         const data = await optimalPriceService(clientId, priceEstimation);
+        // const stringifiedData: string = JSON.stringify(data);
         
-        console.log('Controller res',JSON.stringify(data));
-
-        res.status(200).json(data);
+        res.status(200).json({"status": "OK"});
     } catch (error) {
         next(error)
     }
