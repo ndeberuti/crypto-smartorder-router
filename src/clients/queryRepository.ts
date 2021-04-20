41353aee5e404375ef5babc58befd33200a2bc7d
ndeberuti <ndeberuti@gmail.com>
2021-04-19 22:59:59 -0300

    Remove logging from query repository

--- src/clients/queryRepository.ts
+++ src/clients/queryRepository.ts
@@ -1,13 +1,13 @@
+import { Pair } from "../interfaces/pair";
 import { Side } from "../interfaces/side";
 
-export const insertOrder = (id: string, clientId:string, pair: string, side: Side, volume:string, price:string): string => {
+export const insertOrder = (id: string, clientId:string, pair: Pair, side: Side, volume:string, price:string): string => {
     const query = `insert into orders (id, clientId, pair, side, volume, price) values('${id}', '${clientId}', '${pair}', '${side}', '${volume}', '${price}');`;
 
     return query;
 }
 
 export const getOrderQuery = (id: string): string => {
-    console.log('THIS ID:', id);
     const query = `select * from orders where id = '${id}'`;
 
     return query;
