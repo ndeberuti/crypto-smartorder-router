f929ddca7e5aed799370a870766cfff67b874e82
ndeberuti <ndeberuti@gmail.com>
2021-04-21 09:26:00 -0300

    Remove comment

--- src/server/controller/optimalPriceController.ts
+++ src/server/controller/optimalPriceController.ts
@@ -11,7 +11,6 @@ export const optimalPrice = async (req: express.Request, res: express.Response,
         const clientId = typeof req.headers['x-client-id'] === 'string' ? req.headers['x-client-id']: '';
 
         const data = await optimalPriceService(clientId, priceEstimation);
-        // const stringifiedData: string = JSON.stringify(data);
         
         res.status(200).send(data);
     } catch (error) {
